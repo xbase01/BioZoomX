e53c476ee352b91c2197ffe984629eb10de48c31
xbase01 <judo4christ@gmail.com>
2023-08-23 14:02:56 +0100

    setting up environment

---
+++ bio-insight/src/index.js
@@ -0,0 +1,22 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// Define your custom components here if needed
+// For example, you can create a custom Header component
+
+// Render the App component into the root element
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
